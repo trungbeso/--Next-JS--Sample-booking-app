73364001ddef4dd450edef6ec17ee2b62b21ef06
Kwan Nguyen <kwan@vitex.asia>
2025-11-19 11:33:09 +0700

    add tags and agenda handling in event creation; parse from form data

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -30,6 +30,9 @@ export async function POST(req: NextRequest) {
       );
     }
 
+    let tags = JSON.parse(formData.get("tags") as string);
+    let agenda = JSON.parse(formData.get("agenda") as string);
+
     const arrayBuffer = await file.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
 
@@ -47,7 +50,11 @@ export async function POST(req: NextRequest) {
 
     event.image = (uploadResult as { secure_url: string }).secure_url;
 
-    const createdEvent = await Event.create(event);
+    const createdEvent = await Event.create({
+      ...event,
+      tags: tags,
+      agenda: agenda,
+    });
     return NextResponse.json(
       { message: "Event created successfully", event: createdEvent },
       { status: 201 }
